4531067e9390e6f2922b7c4f60b48e30fdb51ee1
Renan-Storrer <storrerrenan@gmail.com>
2023-03-15 18:06:48 -0300

    rotas e verificações feitas, mas não passa no test

--- src/routers/talkerRouter.js
+++ src/routers/talkerRouter.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const { join } = require('path');
 const crypto = require('crypto');
-const { readFile } = require('../utils/fsCustom');
+const { readFile, writeFile } = require('../utils/fsCustom');
 const { verifyLogin } = require('../middlewares/loginMiddlewar');
+const { verifyAuth, verifyName, verifyAge, verifyTalk, 
+  verifyWatchedAt, verifyRate, verifyRate2 } = require('../middlewares/talkersMiddlewar');
 
 const router = Router();
 const PATH = join(__dirname, '../talker.json');
@@ -26,6 +28,44 @@ const generateToken = () => crypto.randomBytes(8).toString('hex');
 
 router.post('/login', verifyLogin, (_req, res) => {
   res.status(200).json({ token: generateToken() });
+});
+
+router.post('/talker', verifyAuth, verifyName, verifyAge, verifyTalk,
+  verifyWatchedAt, verifyRate, verifyRate2, async (req, res) => {
+    const { body } = req;
+    const talkers = await readFile(PATH);
+    const id = talkers.reduce((acc, cur) => Math.max(acc, Number(cur.id)), 0) + 1;
+    const createdTalker = { id, ...body };
+    const newData = [...talkers, createdTalker];
+    await writeFile(PATH, newData);
+    res.status(201).json(createdTalker);
+  });
+
+router.put('/talker/:id', verifyAuth, verifyName, verifyAge, verifyTalk,
+  verifyWatchedAt, verifyRate, verifyRate2, async (req, res) => {
+    let { id } = req.params;
+    id = Number(id);
+    const { body } = req;
+    const talkers = await readFile(PATH);
+    const findTalker = talkers.find((element) => Number(element.id) === Number(id));
+
+    if (!findTalker) return res.status(404).json({ message: 'Id not found' });
+
+    const index = talkers.findIndex((element) => Number(element.id) === Number(id));
+    const editedTalker = { id, ...body };
+    talkers[index] = editedTalker;
+
+    await writeFile(PATH, talkers);
+    res.status(200).json(editedTalker);
+  });
+
+router.delete('/talker/:id', verifyAuth, async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile(PATH);
+  const filteredTalkers = talkers.filter((element) => Number(element.id) !== Number(id));
+
+  await writeFile(PATH, filteredTalkers);
+  res.status(204).json();
 });
 
 module.exports = router;
